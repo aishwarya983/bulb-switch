a2539303964956babee6e63cd74464a246ab405c
Aishwarya <desaiaishwarya311@gmail.com>
2026-06-07 17:46:57 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const scene = document.getElementById('scene');
-const btn = document.getElementById('switchBtn');
-const label = document.getElementById('statusLabel');
-
-btn.addEventListener('click', () => {
-  scene.classList.toggle('on');
-  label.textContent = scene.classList.contains('on') ? 'ON' : 'OFF';
-});
